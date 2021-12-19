29cd6260793ccafc97d0abf38933cb0cceb24aa2
Michael Chen <pinyuchen1225@gmail.com>
2021-12-20 00:55:49 +0800

    12/20/21

--- function/module.py
+++ function/module.py
@@ -2,17 +2,18 @@
 import sys    #import sys as system:取別名,所以---->print(system.platform)
 print(sys.platform)    #印出系統資訊
 print(sys.maxsize)     #印出最大值
-
-#建立geometry模組,並載入使用
+print(sys.path)
+#建立geometry模組並載入使用
 import geometry
 result=geometry.distance(1,1,5,5)
 print(result)
-result=geometry.slop(1,1,5,5)
-print(result)
 
-#調整搜尋模組的路徑
-import sys
-print(sys.path)        #印出模組的搜尋路徑
+
+
+
+
+
+
 
 
 
