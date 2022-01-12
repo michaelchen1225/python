a162925b7d580900b53a7e3d6047f7347a19880f
Michael Chen <pinyuchen1225@gmail.com>
2022-01-12 15:18:24 +0800

    1/12/22

--- test/test2.py
+++ test/test2.py
@@ -1,7 +1,22 @@
-k=o2.keys()
-count=0
-for i in range(0,len(k)):
-   count+=o.count(k[i])
-   o2[k[i]]+=count
-   print(k[i],"=",count)
-   count=0
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+animals['b'].append('buffalo')
+animals['c'].append('crocodile')
+animals['d'] = ['donkey']
+animals['d'].append('dingo')
+animals['d'].append('dolphin')
+def bigest(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+    returns: The key with the smallest number of values associated with it
+    '''
+    # Your Code Here
+    k=list(animals.keys())
+    v=list(animals.values())
+    c=0
+    b=""
+    for x in range(0,len(k)):
+        if len(v[x])>c:
+            c=len(v[x])
+            b=k[x]
+    return b
+print(bigest(animals))
